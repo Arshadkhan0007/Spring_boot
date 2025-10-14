f5b1a5b450868f976b44d4d9978ac9c6af6fcc28
Arshad <contactkhan0007@gmail.com>
2025-10-14 16:00:43 +0530

    actuators commit-2

---
+++ ActuatorsPractice/src/main/java/com/example/ActuatorsPractice/Service/SimpleService.java
@@ -0,0 +1,11 @@
+package com.example.ActuatorsPractice.Service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class SimpleService {
+
+}
